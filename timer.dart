8bc303d6cf5eb3497032f9a6866e194f167b456b
Jiang Wang <wangjiangb@gmail.com>
2014-01-22 16:52:32 -0600

    add timer

--- timer.dart
+++ timer.dart
@@ -1,5 +1,6 @@
 import 'dart:html';
 import 'dart:convert' show JSON;
+import 'dart:async' show Timer;
 
 class Conference {
   String _name;
@@ -30,7 +31,7 @@ class Conference {
   DateTime get deadline => _deadline;
 }
 
-String DiffString(Duration diff) {
+String diffString(Duration diff) {
   int days = diff.inDays;
   diff -= new Duration(hours: days * Duration.HOURS_PER_DAY);
   int hours = diff.inHours;
@@ -39,19 +40,16 @@ String DiffString(Duration diff) {
   diff -= new Duration( minutes: minutes);
   int seconds = diff.inSeconds;
   return days.toString() + " Days "  + hours.toString() + " Hours " +  minutes.toString() +
-      " Minutes"  + seconds.toString() + " Seconds";
+      " Minutes "  + seconds.toString() + " Seconds";
 }
 
-void main() {
+void updateConfs(Timer _)
+{
   List<Conference> confs = Conference.confs;
-  confs.add(new Conference("ICML", "2014-01-31 23:59:59"));
-  confs.add(new Conference("ECCV", "2014-03-07 23:59:59"));
-  confs.sort((x, y) => x.deadline.compareTo(y.deadline));
-  print(confs.length.toString());
   if (confs.length > 0) {
     querySelector('#timer').style.display = 'block';
     Duration diff = confs[0].deadline.difference(new DateTime.now());
-    querySelector('#counter').text = DiffString(diff);
+    querySelector('#counter').text = diffString(diff);
     querySelector('#conf_name').text = confs[0].name;
   } else {
     return;
@@ -59,11 +57,20 @@ void main() {
   if (confs.length > 1) {
     querySelector('#other').style.display = 'block';
     UListElement confList = querySelector('#deadlines');
+    confList.children.clear();
     for (int i = 1; i < confs.length; ++i) {
       var newConf = new LIElement();
       Duration diff = confs[i].deadline.difference(new DateTime.now());
-      newConf.text = confs[i].name + " Deadline is " + DiffString(diff) + " away.";
+      newConf.text = confs[i].name + " Deadline is " + diffString(diff) + " away.";
       confList.children.add(newConf);
     }
   }
+}
+
+void main() {
+  List<Conference> confs = Conference.confs;
+  confs.add(new Conference("ICML", "2014-01-31 23:59:59"));
+  confs.add(new Conference("ECCV", "2014-03-07 23:59:59"));
+  confs.sort((x, y) => x.deadline.compareTo(y.deadline));
+  Timer timer = new Timer.periodic(new Duration(seconds:1), updateConfs);
 }
